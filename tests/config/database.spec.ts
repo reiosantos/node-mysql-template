a5a3343118a00a686ff6ffa648657a342111b218
santos <ronireiosantos@gmail.com>
2020-09-25 16:43:32 +0300

    - Disable inspection for tests

--- tests/config/database.spec.ts
+++ tests/config/database.spec.ts
@@ -1,16 +1,17 @@
 import DBClient from "@san/config/database";
 import { expect } from 'chai';
 import dbInterface from "@san/config/create-instance";
+import * as sqz from "sequelize";
 
 describe('init db', () => {
   before(() => {
     return (new DBClient(dbInterface)).getClient().then(r => {
       expect(r).to.not.be.undefined;
-      expect(r).to.haveOwnProperty('id');
+      expect(r).to.be.instanceof(sqz.Sequelize);
     })
   });
 
-  it('should update test database', async (done) => {
+  it('should update test database', (done) => {
     done();
   });
 });
